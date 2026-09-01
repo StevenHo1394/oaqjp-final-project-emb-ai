b162102c8a2352b73a837678980615babc90e056
StevenHo1394 <75365123+StevenHo1394@users.noreply.github.com>
2026-09-01 13:57:18 +0800

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,16 +6,20 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
+
+    if response.status_code == 400:
+        result = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
     
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    else:
+        formatted_response = json.loads(response.text)    
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     
-    result = {'anger': anger, 'disgust':disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
-    dominant_emotion = max(result, key=result.get)
-    result['dominant_emotion'] = dominant_emotion
+        result = {'anger': anger, 'disgust':disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
+        dominant_emotion = max(result, key=result.get)
+        result['dominant_emotion'] = dominant_emotion
 
     return result
